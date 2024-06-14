83f90b1789c8a96832ba7817f2af1a604ac25378
deba <debabrata1729@gmail.com>
2024-06-14 08:30:34 +0530

    working condition V0.6

--- main.c
+++ main.c
@@ -1,26 +1,28 @@
+#include "./include/gorilla.h"
+#include "./include/inputv2.h"
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "./include/gorilla.h"
-#include "./include/inputv2.h"
 
 #define ANSI_COLOR_CYAN "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
 #define ESCAPE_KEY '\x1b'
 #define MAXLINE 1000
 
-//Macros for the state variable, which conveys the state the program is in
-#define MENU    0
-#define TYPING  1
-#define RESULT  2
+// Macros for the state variable, which conveys the state the program is in
+#define MENU 0
+#define TYPING 1
+#define RESULT 2
 
-//functions and variables
+// functions and variables
 void display();
-int handleTyping(char, char**); //returns the current length of the string that user is typing
+int handleTyping(
+    char,
+    char **); // returns the current length of the string that user is typing
 int characterControl(char);
 int matchStrings(char *s1, char *s2, int position);
 void resetString(char *string);
@@ -30,34 +32,32 @@ bool startedTyping = false;
 char testString[MAXLINE] = "the quick brown fox jumps over the lazy dog|";
 char displayString[MAXLINE];
 
-
 int main() {
   system("clear");
   display();
 
   char ch;
 
-  char *p = &displayString[0]; //to be noted here that p is still just a character pointer but the way we
-  //pass it in handletyping function is by pointer to a pointer
+  char *p = &displayString[0]; // to be noted here that p is still just a
+                               // character pointer but the way we
+  // pass it in handletyping function is by pointer to a pointer
 
-  //MAIN LOOP
-  while((ch = getch()) != ESCAPE_KEY){ 
-    if(state == 0) {
-      if(ch == '\n') {
+  // MAIN LOOP
+  while ((ch = getch()) != ESCAPE_KEY) {
+    if (state == 0) {
+      if (ch == '\n') {
         state = 1;
       }
-    }
-    else if(state == 1) {
+    } else if (state == 1) {
       int position = handleTyping(ch, &p);
-      if(matchStrings(testString, displayString, position)) {
+      if (matchStrings(testString, displayString, position)) {
         state = 2;
       }
-    }
-    else if(state == 2) {
+    } else if (state == 2) {
       startedTyping = false;
       resetString(displayString);
       p = &displayString[0];
-      if(ch == '\n')
+      if (ch == '\n')
         state = 0;
     }
     display();
@@ -70,46 +70,54 @@ void display() {
   system("clear");
   printf("press Escape to quit the program\n");
   int len = strlen(testString);
-  if(state == 0) {
+  if (state == 0) {
     printFace();
-    printf(ANSI_COLOR_YELLOW"\n\t\t\tWELCOME TO GORILLA TYPE\n\n\t\t\tPress enter to start the test\n"ANSI_COLOR_RESET);
-  } else if(state == 1) {
-    printf(ANSI_COLOR_RED"\n\n \t\t\t%.*s\n" ANSI_COLOR_RESET, len - 1, testString);
+    printf(ANSI_COLOR_YELLOW "\n\t\t\tWELCOME TO GORILLA TYPE\n\n\t\t\tPress "
+                             "enter to start the test\n" ANSI_COLOR_RESET);
+  } else if (state == 1) {
+    printf(ANSI_COLOR_RED "\n\n \t\t\t%.*s\n" ANSI_COLOR_RESET, len - 1,
+           testString);
     printf(ANSI_COLOR_CYAN "\n\n \t\t\t%s\n" ANSI_COLOR_RESET, displayString);
 
-    if(!startedTyping)
-      printf(ANSI_COLOR_GREEN"\n\n\t\t\tPress any key(alphabet or spacebar) to start typing\n");
+    if (!startedTyping)
+      printf(ANSI_COLOR_GREEN
+             "\n\n\t\t\tPress any key(alphabet or spacebar) to start typing\n");
 
-  } else if(state == 2) {
-    printf(ANSI_COLOR_RED"\n\n \t\t\t%.*s\n" ANSI_COLOR_RESET,len - 1, testString);
-    printf(ANSI_COLOR_CYAN "\n\n \t\t\t%s\n" ANSI_COLOR_RESET, displayString);
-    printf(ANSI_COLOR_GREEN"\nSuccess!\nPress enter to go back to main screen\nAnalysis:\n"ANSI_COLOR_RESET);
+  } else if (state == 2) {
+    printf(ANSI_COLOR_RED "\n\n \t\t\t%.*s\n" ANSI_COLOR_RESET, len - 1,
+           testString);
+    printf(ANSI_COLOR_CYAN "\n\n \t\t\t%.*s\n" ANSI_COLOR_RESET, len - 1,
+           displayString);
+    printf(ANSI_COLOR_GREEN "\nSuccess!\nPress enter to go back to main "
+                            "screen\nAnalysis:\n" ANSI_COLOR_RESET);
   }
   printf("\n\n\n");
 }
 
 int characterControl(char ch) {
-  if(isalpha(ch) || ch == ' ' || ch == '\b' || ch == 127)
+  if (isalpha(ch) || ch == ' ' || ch == '\b' || ch == 127)
     return 1;
   else
     return 0;
 }
 
-int handleTyping(char ch, char** p) { //in this function p is a pointer to the pointer which points to the carets position in the string
+int handleTyping(
+    char ch, char **p) { // in this function p is a pointer to the pointer which
+                         // points to the carets position in the string
 
   startedTyping = true;
-  if(characterControl(ch)) {
-    if(ch == '\b' || ch == 127) {//to handle backspace i.e. to delete the most recent char from displayString
+  if (characterControl(ch)) {
+    if (ch == '\b' || ch == 127) { // to handle backspace i.e. to delete the
+                                   // most recent char from displayString
       // displayString[--i] = ' ';
-      if(*p != &displayString[0]){
+      if (*p != &displayString[0]) {
         **p = ' ';
         --(*p);
         **p = '|';
       }
-    }
-    else {
+    } else {
       // displayString[i++] = ch;
-      **p = ch; 
+      **p = ch;
       (*p)++;
       **p = '|';
     }
@@ -120,7 +128,7 @@ int handleTyping(char ch, char** p) { //in this function p is a pointer to the p
 
 void resetString(char *string) {
   int i = 0;
-  while(string[i] != '\0') {
+  while (string[i] != '\0') {
     string[i++] = ' ';
   }
 }
@@ -128,12 +136,12 @@ void resetString(char *string) {
 int matchStrings(char *s1, char *s2, int position) {
   // char s3[position];
   // strcpy(s3, s2);
-  return (strcmp(s1,s2) == 0);
+  return (strcmp(s1, s2) == 0);
 }
 
 void printFace() {
   // Print smiley face ASCII art
-  printf(ANSI_COLOR_YELLOW"\t\t\t");
+  printf(ANSI_COLOR_YELLOW "\t\t\t");
   printf("   _____\n\t\t\t");
   printf("  /     \\\n\t\t\t");
   printf(" | () () |\n\t\t\t");
